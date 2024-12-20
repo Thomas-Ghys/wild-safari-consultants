7bc00ad5bf2975006dfde2aaebc5f9044623e1e2
Thomas Ghys <thomas-ghys@outlook.com>
2024-12-21 00:10:42 +0100

    add linter rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 const nx = require('@nx/eslint-plugin');
+const html = require('@html-eslint/eslint-plugin');
 
 module.exports = [
 	...nx.configs['flat/base'],
@@ -171,8 +172,44 @@ module.exports = [
 		}
 	},
 	{
-		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-		// Override or add rules here
-		rules: {}
+		...html.configs['flat/recommended'],
+		files: ['**/*.html'],
+		rules: {
+			'@html-eslint/no-duplicate-attrs': 'error',
+			'@html-eslint/no-duplicate-id': 'error',
+			'@html-eslint/no-extra-spacing-text': 'error',
+			'@html-eslint/no-inline-styles': 'error',
+			'@html-eslint/no-obsolete-tags': 'error',
+			'@html-eslint/no-script-style-type': 'error',
+			'@html-eslint/no-target-blank': 'error',
+			'@html-eslint/require-button-type': 'error',
+			'@html-eslint/require-closing-tags': 'error',
+			'@html-eslint/require-doctype': 'error',
+			'@html-eslint/require-li-container': 'error',
+			'@html-eslint/require-meta-charset': 'error',
+			'@html-eslint/no-multiple-h1': 'error',
+			'@html-eslint/require-lang': 'error',
+			'@html-eslint/require-meta-description': 'error',
+			'@html-eslint/require-title': 'error',
+			'@html-eslint/no-abstract-roles': 'error',
+			'@html-eslint/no-accesskey-attrs': 'error',
+			'@html-eslint/no-aria-hidden-body': 'error',
+			'@html-eslint/no-non-scalable-viewport': 'error',
+			'@html-eslint/no-positive-tabindex': 'error',
+			'@html-eslint/no-skip-heading-levels': 'error',
+			'@html-eslint/require-frame-title': 'error',
+			'@html-eslint/require-img-alt': 'error',
+			'@html-eslint/require-meta-viewport': 'error',
+			'@html-eslint/attrs-newline': 'error',
+			'@html-eslint/element-newline': 'error',
+			'@html-eslint/id-naming-convention': ['error', 'kebab-case'],
+			'@html-eslint/indent': ['error', 'tab'],
+			'@html-eslint/lowercase': 'error',
+			'@html-eslint/no-extra-spacing-attrs': 'error',
+			'@html-eslint/no-multiple-empty-lines': 'error',
+			'@html-eslint/no-trailing-spaces': 'error',
+			'@html-eslint/quotes': ['error', 'single'],
+			'@html-eslint/sort-attrs': 'error'
+		}
 	}
 ];
