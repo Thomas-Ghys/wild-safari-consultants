bc5e73530c29ff8bc2dbd887351b52d2e6a92658
Thomas Ghys <thomas-ghys@outlook.com>
2024-12-20 23:17:47 +0100

    Implement insta contact card

--- libs/common/common-data/src/lib/stores/contact.store.ts
+++ libs/common/common-data/src/lib/stores/contact.store.ts
@@ -2,10 +2,12 @@ import {signalStore, withState} from '@ngrx/signals';
 
 export type ContactStore = {
 	mobile: string;
+	instagram: string;
 };
 
 const initialContactState: ContactStore = {
-	mobile: '+27631751299'
+	mobile: '+27631751299',
+	instagram: '@wildsafari.consultants'
 };
 
 export const ContactStore = signalStore(
