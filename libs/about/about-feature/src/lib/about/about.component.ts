5461e629e723179ffb9263052bce59b612ae04f7
Thomas Ghys <thomas-ghys@outlook.com>
2024-12-22 23:02:02 +0100

    implement about feature

--- libs/about/about-feature/src/lib/about/about.component.ts
+++ libs/about/about-feature/src/lib/about/about.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FeatureContainerComponent} from '@thomas-ghys.eu/common-ui';
+import { BreakpointDirective } from '@thomas-ghys.eu/common-utils';
 
 @Component({
 	selector: 'lib-about',
 	standalone: true,
-	imports: [CommonModule, FeatureContainerComponent],
+	imports: [CommonModule, FeatureContainerComponent, BreakpointDirective],
 	templateUrl: './about.component.html',
-	styleUrl: './about.component.scss'
+	styleUrl: './about.component.scss',
 })
 export class AboutComponent {}
