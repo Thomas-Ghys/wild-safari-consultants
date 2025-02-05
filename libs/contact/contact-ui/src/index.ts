48ef5454060ddb6081e1dcf19d9d1aa9a3a1ca56
Thomas Ghys <thomas-ghys@outlook.com>
2025-02-05 18:18:59 +0100

    Code cleanup from refactor

--- libs/contact/contact-ui/src/index.ts
+++ libs/contact/contact-ui/src/index.ts
@@ -1,4 +1,1 @@
-export * from './lib/email-contact-card/email-contact-card.component';
-export * from './lib/link-contact-card/link-contact-card.component';
-export * from './lib/qr-contact-card/qr-contact-card.component';
 export * from './lib/contact-card/contact-card.component';
