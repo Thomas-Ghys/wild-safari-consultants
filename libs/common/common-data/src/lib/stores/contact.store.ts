9575006c94f1b5c9b5124ff5c3f53dae6a2fdb18
Thomas Ghys <thomas-ghys@outlook.com>
2025-02-05 17:35:36 +0100

    Change business email

--- libs/common/common-data/src/lib/stores/contact.store.ts
+++ libs/common/common-data/src/lib/stores/contact.store.ts
@@ -9,7 +9,7 @@ export type ContactStore = {
 const initialContactState: ContactStore = {
 	mobile: '+27631751299',
 	instagram: 'wildsafari.consultants',
-	email: 'botserakganya05@gmail.com'
+	email: 'wild-safari-consultants@outlook.com'
 };
 
 export const ContactStore = signalStore(
